2845c6d04e403f32c8105d71618cbc91ef0a6cc2
Yeram <rlaexram@pukyong.ac.kr>
2026-03-09 17:07:59 +0900

    오류수정

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDTCAmwR1ldaWIAXPGV_qaXIF68ngmYHDI",
@@ -13,3 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
